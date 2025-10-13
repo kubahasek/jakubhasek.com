a1bc5f22e0b59c1fe22908c1fba5dd3882e67311
Jakub Hašek <me@jakubhasek.com>
2025-10-13 21:00:05 +0200

    docker

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ import rehypeHighlight from "rehype-highlight";
 const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  // Enable standalone output for Docker
+  output: "standalone",
   // Optionally, add any other Next.js config below
 };
 
